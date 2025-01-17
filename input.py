7bcf932523dadd08511e5da7d8c754b55853bbbe
Thestaloz <oskr-75@hotmail.com>
2025-01-16 20:38:01 -0600

    creamos el archivo input

---
+++ input.py
@@ -0,0 +1,9 @@
+#print(input("Dime tu nombre: "))
+#print(input("Dime tu apellido: "))
+
+#print("Tu nombre es " + input("Dime tu nombre: ") + " " + input("Dime tu appellido: "))
+
+x = input("Dime tu nombre: ")
+y = input("Dime tu apellido: ")
+
+print(f"Tu nombre es {x} y tu apellido es {y}")
