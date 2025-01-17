e8db30cb9c0904e9ce7b9a1a0e4916c7f5205fc8
Thestaloz <oskr-75@hotmail.com>
2025-01-16 20:58:02 -0600

    colocamos todo en la carpeta del dia 1

---
+++ Dia_1/proyecto_dia1.py
@@ -0,0 +1,4 @@
+print("Bienvenido al programa para crear un nombre aleatorio para tu cerveza con 2 preguntas.\nPor favor contesta las siguientes pregunrtas: \n ")
+
+print("El nombre de tu cerveza es: " + input("¿Cual es tu color favorito? ") + " " + input("¿cual es el nombre tu perro? "))
+
