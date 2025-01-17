83376d571d02b75fc33bb01684188d47d7e31155
Thestaloz <oskr-75@hotmail.com>
2025-01-16 20:04:22 -0600

    creamos un archivo para explicar los strings

---
+++ string1.py
@@ -0,0 +1,5 @@
+print("me llamo \"kevin\"")
+print("hola esta es una linea \ny esta es otra linea")
+print("\t tabulamos letras con este comando")
+print("this isn't a number")
+print("este \\ es una barra invertida")
