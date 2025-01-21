65ff96391a1782ebca08a700cec38e33b2c0de67
Thestaloz <oskr-75@hotmail.com>
2025-01-21 00:26:16 -0600

    agregamos archivo de conversiones

---
+++ Dia_2/conversiones.py
@@ -0,0 +1,16 @@
+# num1 = 5.8
+
+# print(num1)
+# print(type(num1))
+
+# num2 = int(num1)
+
+# print(num2)
+# print(type(num2))
+
+edad = input("Ingresa tu edad: ")
+edad = int(edad)
+
+nueva_edad = edad + 1
+
+print(nueva_edad) 
