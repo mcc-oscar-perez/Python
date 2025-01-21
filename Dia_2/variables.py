dfc7a2c0f9be5a1abc78eaeec5b29c39cd7c0926
Thestaloz <oskr-75@hotmail.com>
2025-01-20 23:46:59 -0600

    creamos segunda carpeta con primer practica

---
+++ Dia_2/variables.py
@@ -0,0 +1,10 @@
+nombre = "Juan"
+print(nombre)
+
+#re asignamos otro valor a la variable nombre
+nombre = "Laura"
+print(nombre)
+
+# asignamos a una variable un input 
+nombre2 = input("dime tu nombre: ")
+print("tu nombre es " + nombre2)
