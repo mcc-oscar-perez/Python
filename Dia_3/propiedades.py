298972a31417677659a8a77db5d79481d2e4d98b
Thestaloz <oskr-75@hotmail.com>
2025-01-28 15:08:46 -0600

    agregamos archivo propiedades.py

---
+++ Dia_3/propiedades.py
@@ -0,0 +1,15 @@
+# poema = """ahora si todo saldra bien , y lo que no
+# pues los superaremos, y si no, pues tambien"""
+
+# print(poema)
+
+# poema = """ahora si todo saldra bien , y lo que no
+# pues los superaremos, y si no, pues tambien"""
+
+# print('ahora' not in poema)
+
+
+poema = """ahora si todo saldra bien , y lo que no
+pues los superaremos, y si no, pues tambien"""
+
+print(len(poema))
