deef31cc12443bb203e4e27e741cbcdae4e59740
Thestaloz <oskr-75@hotmail.com>
2025-02-07 23:36:17 -0600

    agregamos comentarios en print

--- Carpeta_1/print.py
+++ Carpeta_1/print.py
@@ -2,3 +2,8 @@
 # practice exercises with "print"
 
 print("Hola mundo te digo 'hola' ")
+print("me llamo \"kevin\"")
+print("hola esta es una linea \ny esta es otra linea")
+print("\t tabulamos letras con este comando")
+print("this isn't a number")
+print("este \\ es una barra invertida")
