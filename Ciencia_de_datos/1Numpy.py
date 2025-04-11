ae7712aa9796ea43d1c20acb07a04ada4cc8c354
Thestaloz <oskr-75@hotmail.com>
2025-04-10 18:16:39 -0600

    agregamos informacion a archivo numpy

--- Ciencia_de_datos/1Numpy.py
+++ Ciencia_de_datos/1Numpy.py
@@ -1,4 +1,4 @@
-# Crear una matriz numpy 
+# How to create a numpy array and print some properties of the array
 
 import numpy as np 
 
@@ -8,14 +8,41 @@ print(array)
 print(type(array))
 print(array.shape)
 
-
-# Hcemos una matriz de 2x3 
+#%%
+# Create a 2D numpy array (matrix) with 2 rows and 3 columns
+# pritnt some properties of the matrix
+# and print some specific elements of the matrix 
 
 import numpy as np
 
 matriz = np.array([[1,2,3],[4,5,6]])
 print(matriz)
 print(matriz.shape)
-print("f first row is: {matriz[0]}")
-print("f last row is: {matriz[1]}")
+print(f" first row is: {matriz[0]}")
+print(f" last row is: {matriz[1]}")
+print(f" Specific element is: {matriz[0,1]}") # 0 is the row and 1 is the column
+print(f"Whole column is: {matriz[ : , 2]}") # 2 is the column
+# %%
+
+# Do mathematical operations with numpy arrays
+
+import numpy as np 
+
+array1 = np.array([1,2,3])
+array2 = np.array([4,5,6])
+
+print(array1 + array2) # sum of the two arrays
+print(array1 - array2) # subtraction of the two arrays
+print(array1 * array2) # multiplication of the two arrays
+print(array1 / array2) # division of the two arrays
+print(array1 ** 2) # square of the first array
+print(array1 * 2) # multiplication of the first array by 2
+print(array1 + 2) # sum of the first array by 2
+print(array1 * array2 + 2) # multiplication of the two arrays and sum by 2
+print(array1 * array2 - 2) # multiplication of the two arrays and subtraction by 2
+print(array1 * array2 / 2) # multiplication of the two arrays and division by 2
+print(array1 * array2 ** 2) # multiplication of the first array by the square of the second array
+print(array1 * array2 + 2) # multiplication of the two arrays and sum by 2
+
+
 # %%
