c7cbc5bd9ecc1c8db21ecf403a3fac6eb411c2c8
Thestaloz <oskr-75@hotmail.com>
2025-04-10 18:15:19 -0600

    agreagmos carpeta nueva y archivo de numpy

---
+++ Ciencia_de_datos/1Numpy.py
@@ -0,0 +1,21 @@
+# Crear una matriz numpy 
+
+import numpy as np 
+
+lista = [1,2,3,4,5]
+array = np.array(lista)
+print(array)
+print(type(array))
+print(array.shape)
+
+
+# Hcemos una matriz de 2x3 
+
+import numpy as np
+
+matriz = np.array([[1,2,3],[4,5,6]])
+print(matriz)
+print(matriz.shape)
+print("f first row is: {matriz[0]}")
+print("f last row is: {matriz[1]}")
+# %%
