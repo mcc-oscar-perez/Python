946d545db81a40cd4514b900aea72ca01a85434e
Thestaloz <oskr-75@hotmail.com>
2025-05-28 17:42:52 -0600

    Creamos la carpeta 8, gitignore y subimos  ejercicios

---
+++ Carpeta_8/Package/Pakage2/Saludo.py
@@ -0,0 +1,2 @@
+def saludar():
+    print("Hola esto esta siendo impreso desde un paquete guarado en otro paquete")
---
+++ Carpeta_8/module0.py
@@ -0,0 +1,11 @@
+# import modules 
+from module import saludar
+from Package import suma_resta
+from Package.Pakage2 import Saludo
+
+
+#Testing the imported functions from packages or modules
+saludar()
+suma_resta.resta(15,2)
+suma_resta.suma(20,55)
+Saludo.saludar()
