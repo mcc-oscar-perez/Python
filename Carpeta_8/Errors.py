95aa0b51c4dfdb15ae6097484feb41e23bf5ff5c
Thestaloz <oskr-75@hotmail.com>
2025-05-30 19:36:20 -0600

    agregamos contenido para practicar pylint

--- Carpeta_8/Errors.py
+++ Carpeta_8/Errors.py
@@ -47,3 +47,35 @@ pedir_numero()
     
 # %%
 #Practical ecercises 
+
+# 1
+def suma(num1,num2):
+    try:
+        print(num1+num2)
+    except:
+        print("Error inesperado")
+
+# 2
+
+def cociente(num1,num2):
+    try:
+        print(num1/num2)
+    except TypeError:
+        print("Los argumentos a ingresar deben ser números")
+    except ZeroDivisionError:
+        print("El segundo argumento no debe ser cero")
+        
+# 3 
+
+def abrir_archivo(nombre_archivo):
+    try:
+        archivo = open(nombre_archivo)
+    except FileNotFoundError:
+        print("El archivo no fue encontrado")
+    except:
+        print("Error desconocido")
+    else:
+        print("Abriendo exitosamente")
+    finally:
+        print("Finalizando ejecución")
+
