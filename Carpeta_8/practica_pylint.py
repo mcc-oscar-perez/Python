0ddf4a99850abbf83aa7751e4e26e018078fab9e
Thestaloz <oskr-75@hotmail.com>
2025-05-31 18:02:27 -0600

    agregamos un archivo con errores para corregirlo con pylint

---
+++ Carpeta_8/practica_pylint.py
@@ -0,0 +1,9 @@
+"""Este archivo es para poner a prueba la libreria Pylint
+This file is from to probe the librery Pylint"""
+
+def sumar(numero1, numero2):
+    """Devuelve la suma de 2 numeros"""
+    return numero1+numero2
+
+resultado_suma = sumar(5,7)
+print(resultado_suma)
