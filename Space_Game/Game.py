5023df6e8f8b780f7b9593ce3adf4847aa3b2502
Thestaloz <oskr-75@hotmail.com>
2025-06-27 22:31:56 -0600

    Agregamos interaccion de teclas para la nave

--- Space_Game/Game.py
+++ Space_Game/Game.py
@@ -8,16 +8,18 @@ pantalla = pygame.display.set_mode((800, 600))
 
 #Titiel and icon
 pygame.display.set_caption("Invasión Alienígena")
-icono = pygame.image.load("D:\Cursos\PYTHON\Carpeta_10\ovni.png")
+icono = pygame.image.load("D:\Cursos\PYTHON\Space_Game\ovni.png")
 pygame.display.set_icon(icono)
 
 # Player
-img_jugador = pygame.image.load("D:\Cursos\PYTHON\Carpeta_10\cohete_espacial.png")
+img_jugador = pygame.image.load("D:\Cursos\PYTHON\Space_Game\cohete_espacial.png")
 jugador_x = 368
 jugador_y = 536
+jugador_x_change = 0
 
-def jugador():
-    pantalla.blit(img_jugador, (jugador_x, jugador_y))
+
+def jugador(x,y):
+    pantalla.blit(img_jugador, (x,y))
 
 # This is the loop that will keep the game running
 se_ejecuta = True
@@ -29,8 +31,24 @@ while se_ejecuta:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             se_ejecuta = False
-    
-    jugador()  # Call the function to draw the player
+        # Here we program the controls for the player 
+        if event.type == pygame.KEYDOWN:
+            
+            if event.key == pygame.K_LEFT:
+                jugador_x_change = -0.1
+            if event.key == pygame.K_RIGHT:
+                jugador_x_change = 0.1
+                
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                jugador_x_change = 0
+                
+
+
+    # Update the player's position
+    jugador_x += jugador_x_change
+    jugador(jugador_x,jugador_y)  # Call the function to draw the player
     
     pygame.display.update() 
+
 
