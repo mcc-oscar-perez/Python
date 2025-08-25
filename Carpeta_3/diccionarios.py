501eff18073d44a47fc9950662c77b1707108d23
Thestaloz <oskr-75@hotmail.com>
2025-08-24 22:46:46 -0600

    Acutualizacion a diccionarios

--- Carpeta_3/diccionarios.py
+++ Carpeta_3/diccionarios.py
@@ -1,25 +1,33 @@
 # Diccionarios
 # Dictionaries
 
+#%%
 diccionario = {'c1': 'valor1', 'c2': 'valor2', 'c3': 'valor3'}
 print(diccionario)
 
+#%%
 restultado = diccionario['c2']
 print(restultado)
 
+#%%
 cliente = {"nombre": "Juan", "edad": 28, "direccion": "Calle 13 # 12-34"}
 restultado2 = cliente['edad']
 print(restultado2) 
 
+#%%
 dic = {"c1": 55, "c2": [10, 20, 30], "c3": {"s1":100, "s2":200}}
 print(dic['c2'][1])
 
+#%%
 dic2 = {"c1": ["a", "b","c"], "c2": ["d", "e","f"]}
 print(dic2["c2"][1].upper())
 
+#%%
 diccionario['c4'] = 'valor4'
 print(diccionario)
 
+#%%
 print(diccionario.keys())
 print(diccionario.values())
-print(dic.items())
+print(diccionario.items())
+# %%
