13ac483e767e8100eb2482816a50f889bed82759
Thestaloz <oskr-75@hotmail.com>
2025-10-13 16:23:26 -0600

    assistent 1

---
+++ IA/AI_Assistant.py
@@ -0,0 +1,8 @@
+import pyttsx3 # sistema que se encarga de hablar con nosotros 
+import speech_recognition as sr # Reconoce la voz 
+import pywhatkit # permite que el sistema pueda abrir sitios web
+import yfinance as yf# revisar la bolsa para revisar acciones 
+import pyjoks # contar chistes 
+import webbrowser # manejar navegador de internet
+import datetime # fecha 
+import wikipedia
